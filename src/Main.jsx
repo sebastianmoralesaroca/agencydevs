388ede8fa5ee6cc1caf4137e80c45325b541cb6e
seba.developer <cmoralesaroca@gmail.com>
2023-10-11 18:54:39 -0300

    Primer Commit

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Name, Last } from "./js/name";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Name />
+    <h1>hola</h1>
+    <Last />
+  </>
+);
---
+++ src/js/name.jsx
@@ -0,0 +1,17 @@
+export function Name() {
+    let x = "seba mor";
+    return (
+      <div>
+        <h1>Name</h1>
+        <p>{x}</p>
+      </div>
+    );
+  }
+
+export function Last() {
+  return (
+    <div>
+      <h1>Morales</h1>
+    </div>
+  )
+}
